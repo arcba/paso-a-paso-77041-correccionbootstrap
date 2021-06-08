fa89a1e797bda611a19824d32144d35b0d84a061
arcba <alejandroreyc@yahoo.com.ar>
2021-06-08 19:43:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# paso-a-paso-77041-correccionbootstrap
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/paso-a-paso-77041-correccionbootstrap)
---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
